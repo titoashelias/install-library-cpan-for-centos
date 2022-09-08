28beecc552669cb4cf66fe7a51f1196fb79ad70d
Good Grand Father <98451166+titoashelias@users.noreply.github.com>
2022-09-08 09:14:54 +0300

    uploade script

---
+++ script.sh
@@ -0,0 +1,331 @@
+yum update -y &&
+yum install git -y &&
+yum install wget -y &&
+yum install gcc* -y &&
+yum install glib* -y &&
+yum install glibc* -y &&
+yum install python* -y &&
+yum install perl* -y &&
+yum install bzip2 -y &&
+yum install tar -y &&
+yum install unzip -y &&
+yum install xz-utils -y &&
+yum install perl-Template-Toolkit -y &&
+yum install gcc make cmake -y &&
+yum install glibc.i686 -y &&
+yum install Parallel::ForkManager -y &&
+yum install perl-CPAN &&
+yum install perl-YAML perl-CPAN-DistnameInfo perl-Test-Mock-LWP gcc-c++ cpan -y &&
+yum install perl-CPAN -y &&
+yum install build-essential -y &&
+yum install gfortran libblas-dev liblapack-dev libatlas-dev python-virtualenv -y &&
+yum install libnet-ssleay-perl  -y &&
+yum install libparallel-forkmanager-perl  -y &&
+yum install perl-Parallel-ForkManager.noarch  -y &&
+yum install php-cli -y &&
+yum install php-curl -y &&
+yum install python-pip -y &&
+yum install perl-YAML perl-CPAN-DistnameInfo perl-Test-Mock-LWP gcc-c++ cpan --skip-broken &&
+yum install php -y &&
+yum install python -y &&
+yum install zlib-devel  -y &&
+yum install libswitch-perl -y &&
+yum install perl -y &&
+yum install perl-IO-Socket-SSL -y &&
+yum install perl-Net-SIP.noarch -y &&
+yum install dh-make-perl dpkg-dev -y &&
+yum install gcc -y &&
+yum install gcc php-devel php-pear libssh2 libssh2-devel -y &&
+yum install glibc-devel.i686 glibc.i686 -y &&
+yum install glibc.i686 -y && yum install glibc.i386 -y &&
+yum install libdbi-perl libdbd-mysql-perl libdigest-md5-perl libdigest-sha1-perl libimage-size-perl libmime-lite-perl libmime-perl libcompress-zlib-perl libmime-base64-perl liburi-perl libhtml-tagset-perl libhtml-parser-perl libwww-perl libwww-perl libgd-perl libmailtools-perl libunicode-maputf8-perl libstorable-perl libtime-hires-perl
+yum install libdbi-perl libdbd-mysql-perl libdigest-md5-perl libdigest-sha1-perl libimage-size-perl libmime-lite-perl libmime-perl libcompress-zlib-perl libmime-base64-perl liburi-perl libhtml-tagset-perl libhtml-parser-perl libwww-perl libwww-perl libgd-perl libmailtools-perl libunicode-maputf8-perl libstorable-perl libtime-hires-perl  -y &&
+yum install libdbi-perl libdbd-mysql-perl libdigest-md5-perl libdigest-sha1-perl libimage-size-perl libmime-lite-perl libmime-perl libcompress-zlib-perl libmime-base64-perl liburi-perl libhtml-tagset-perl libhtml-parser-perl libwww-perl libwww-perl libgd-perl libmailtools-perl libunicode-maputf8-perl libstorable-perl libtime-hires-perl -y &&
+yum install libmath-gmp-perl -y &&
+yum install libnet-ssleay-perl -y &&
+yum install libcrypt-ssleay-perl -y &&
+yum  install perl-IO-Socket-SSL -y &&
+yum  install httpd rrdtool fping echoping curl perl perl-Net-Telnet perl-Net-DNS perl-LDAP perl-libwww-perl perl-RadiusPerl perl-IO-Socket-SSL perl-Socket6 perl-CGI-SpeedyCGI -y &&
+yum install libnet-lite-ftp-perl -y &&
+yum install libnet-perl libgd-graph-perl libproc-process-perl librpc-xml-perl libsoap-lite-perl libxml-rss-perl libstring-crc32-perl -y &&
+yum install libnet-perl libgd-graph-perl libproc-process-perl librpc-xml-perl libsoap-lite-perl libxml-rss-perl libstring-crc32-perl -y &&
+yum install libnet-telnet-cisco-perl libexpect-perl libxml-simple-perl -y &&
+yum install --skip-broken glibc.i686 arts.i686 audiofile.i686 bzip2-libs.i686 cairo.i686 cyrus-sasl-lib.i686 dbus-libs.i686 directfb.i686 esound-libs.i686 fltk.i686 freeglut.i686 gtk2.i686 hal-libs.i686 imlib.i686 lcms-libs.i686 lesstif.i686 libacl.i686 libao.i686 libattr.i686 libcap.i686 libdrm.i686 libexif.i686 libgnomecanvas.i686 libICE.i686 libieee1284.i686 libsigc++20.i686 libSM.i686 libtool-ltdl.i686 libusb.i686 libwmf.i686 libwmf-lite.i686 libX11.i686 libXau.i686 libXaw.i686 libXcomposite.i686 libXdamage.i686 libXdmcp.i686 libXext.i686 libXfixes.i686 libxkbfile.i686 libxml2.i686 libXmu.i686 libXp.i686 libXpm.i686 libXScrnSaver.i686 libxslt.i686 libXt.i686 libXtst.i686 libXv.i686 libXxf86vm.i686 lzo.i686 mesa-libGL.i686 mesa-libGLU.i686 nas-libs.i686 nss_ldap.i686 cdk.i686 openldap.i686 pam.i686 popt.i686 pulseaudio-libs.i686 sane-backends-libs-gphoto2.i686 sane-backends-libs.i686 SDL.i686 svgalib.i686 unixODBC.i686 zlib.i686 compat-expat1.i686 compat-libstdc++-33.i686 openal-soft.i686 alsa-oss-libs.i686 redhat-lsb.i686 alsa-plugins-pulseaudio.i686 alsa-plugins-oss.i686 alsa-lib.i686 nspluginwrapper.i686 libXv.i686 libXScrnSaver.i686 qt.i686 qt-x11.i686 pulseaudio-libs.i686 pulseaudio-libs-glib2.i686 alsa-plugins-pulseaudio.i686 -y &&
+yum install build-essential autoconf automake curl lua5.1 linux-headers-generic libncurses-dev zlib1g-dev libssl-dev libxml2-dev libsqlite3-dev uuid-dev libspeex-dev libspeexdsp-dev libogg-dev libvorbis-dev libasound2-dev portaudio19-dev libcurl4-gnutls-dev libpq-dev unixodbc-dev libsqlite3-dev libmysqlclient-dev libneon27-dev libgmime-2.6-dev libusb-dev liblua5.1-0-dev libgtk2.0-dev libmysqlclient-dev libbluetooth-dev libradiusclient-ng-dev freetds-dev libsnmp-dev libiksemel-dev libcorosync-dev libnewt-dev libpopt-dev libical-dev libspandsp-dev libjack-dev libresample-dev libc-client-dev binutils-dev libsrtp-dev libgsm1-dev libedit-dev libjansson-dev libldap-dev libxslt1-dev libmyodbc doxygen subversion git mpg123 e2fsprogs php5 php5-curl sqlite3 uuid php5-cli php5-mysql php5-gd php-pear sox bison flex tftpd mailutils nano ntp apache2 mysql-client mysql-server postfix -y &&
+yum install automake gcc gcc-c++ ncurses-devel openssl-devel libxml2-devel unixODBC-devel libcurl-devel libogg-devel libvorbis-devel speex-devel spandsp-devel freetds-devel net-snmp-devel iksemel-devel corosynclib-devel newt-devel popt-devel libtool-ltdl-devel lua-devel sqlite-devel radiusclient-ng-devel portaudio-devel libresample-devel neon-devel libical-devel openldap-devel gmime-devel mysql-devel bluez-libs-devel jack-audio-connection-kit-devel gsm-devel libedit-devel libuuid-devel jansson-devel libsrtp-devel git subversion libxslt-devel kernel-devel audiofile-devel gtk2-devel libtiff-devel libtermcap-devel bison php php-mysql php-process php-pear php-mbstring php-xml php-gd tftp-server httpd sox tzdata mysql-connector-odbc mysql-server fail2ban -y &&
+yum install gcc gcc-c++ lynx bison mysql-devel mysql-server php php-mysql php-pear php-mbstring tftp-server httpd make ncurses-devel libtermcap-devel sendmail sendmail-cf caching-nameserver sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git subversion kernel-devel php-process crontabs cronie cronie-anacron -y &&
+yum install -y make wget openssl-devel ncurses-devel newt-devel libxml2-devel kernel-devel gcc gcc-c++ sqlite-devel -y &&
+yum search ssl zlib readline &&
+yum groupinstall "Development Tools" -y &&
+yum install glib2.i686 qt.i686 zlib.i686 alsa-lib.i686 libX11.i686 libXv.i686 libXScrnSaver.i686 -y &&
+yum install glibc.i686 alsa-lib.i686 libXv.i686 libXScrnSaver.i686 qt.i686 qt-x11.i686 -y &&
+yum install alsa-lib.i686 -y &&
+yum install glibc.i686 alsa-lib.i686 libXv.i686 -y &&
+yum install libXScrnSaver.i686 libSM.i686 libXi.i686 libXrender.i686 -y &&
+yum install libXrandr.i686 freetype.i686 fontconfig.i686 zlib.i686 -y &&
+yum install glib2.i686 libstdc++.i686 -y &&
+yum install pulseaudio-libs.i686 alsa-plugins-pulseaudio.i686 -y &&
+yum install libasound.so.2 libXv.so.1 libXss.so.1 libSM.so.6 libXi.so.6 libXrender.so.1 libXrandr.so.2 libfreetype.so.6 libfontconfig.so.1 libz.so.1 libgthread-2.0.so.0 libstdc++.so.6 libQtDBus.so.4 libQtGui.so.4 pulseaudio-libs.i686 -y &&
+yum install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm -y &&
+yum install qtwebkit.i686 webkitgtk.i686 -y &&
+yum install glibc.i686 alsa-lib.i686 libXv.i686 libXScrnSaver.i686 qt.i686 gtk2-engines.i686 PackageKit-gtk-module.i686 libcanberra.i686 libcanberra-gtk2.i686 -y &&
+yum install bind-utils -y &&
+yum info glibc.i686 -y &&
+yum install libstdc++.i686 -y &&
+yum install ncurses-libs.i686 -y &&
+yum install zlib.i686 -y &&
+yum install glibc.i686 glibc-devel.i686 libstdc++.i686  zlib-devel.i686 ncurses-devel.i686  libX11-devel.i686 libXrender.i686  libXrandr.i686 -y &&
+yum install glibc.i686; yum install pam.i686 -y &&
+yum upgrade libgcc.x86_64 pam.x86_64 -y &&
+yum install pam.i686  -y &&
+yum info glibc.i686 -y &&
+yum install bind-utils -y &&
+yum install bind-utils -y &&
+yum install libstdc++.i686 -y &&
+yum install ncurses-libs.i686 -y &&
+yum install zlib.i686 -y &&
+yum install mutt -y &&
+yum install ssmtp -y &&
+yum install cyrus-sasl -y &&
+yum install cyrus-imapd -y &&
+yum install glibc.i686 glibc-devel.i686 libstdc++.i686  zlib-devel.i686 ncurses-devel.i686  libX11-devel.i686 libXrender.i686  libXrandr.i686 -y &&
+yum install glibc.i686; yum install pam.i686 -y &&
+yum upgrade libgcc.x86_64 pam.x86_64 -y &&
+yum install pam.i686 -y &&
+yum install libXm.so.* -y &&
+yum install java-1.7.0-openjdk -y &&
+yum install perl-CPAN -y &&
+cpan Parallel::ForkManager &&
+yum install build-essential -y  &&
+yum install libparallel-forkmanager-perl -y  &&
+yum install perl-Parallel-ForkManager.noarch -y  &&
+yum install perl-Params-Validate -y &&
+rpm -ql perl-Params-Validate -y &&
+yum install perl-ExtUtils-MakeMaker perl-Test-Harness -y &&
+yum install perl-CGI -y &&
+perl -MCPAN -e 'install Parallel::ForkManager' &&
+wget http://www.cpan.org/src/5.0/perl-5.20.1.tar.gz &&
+tar -xzf perl-5.20.1.tar.gz &&
+cd perl-5.20.1 &&
+./Configure -des -Dprefix=$HOME/localperl &&
+make &&
+make test &&
+make install &&
+wget --no-check-certificate https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz &&
+tar xf Python-2.7.6.tar.xz &&
+cd Python-2.7.6 &&
+./configure --prefix=/usr/local &&
+make && make altinstall &&
+wget http://www.pjsip.org/release/2.1/pjproject-2.1.tar.bz2 &&
+tar xvfj pjproject-2.1.tar.bz2 &&
+cd pjproject-2.1.0 &&
+./configure  &&
+make dep && make &&
+make install &&
+ldconfig &&
+ldconfig -p | grep pj &&
+cd ./pjsip-apps/src/python &&
+python setup.py install &&
+yum install glibc-devel.i686 glibc.i686 -y &&
+wget --no-check-certificate http://goo.gl/7N3tws &&
+yum install shc-3.8.9-1.1.i386.rpm --nogpgcheck &&
+yum install perl-IO-Socket-SSL -y  &&
+yum install httpd rrdtool fping echoping curl perl perl-Net-Telnet perl-Net-DNS perl-LDAP perl-libwww-perl perl-RadiusPerl perl-IO-Socket-SSL perl-Socket6 perl-CGI-SpeedyCGI -y  &&
+wget http://search.cpan.org/CPAN/authors/id/S/SA/SALVA/Net-OpenSSH-0.64.tar.gz &&
+tar xvzf Net-OpenSSH-0.64.tar.gz &&
+cd Net-OpenSSH-0.64 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/S/SU/SULLR/Net-SIP-0.687.tar.gz &&
+tar xvzf Net-SIP-0.687.tar.gz &&
+cd Net-SIP-0.687 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/M/MS/MSCHILLI/LWP-Protocol-https-6.06.tar.gz &&
+tar xvzf LWP-Protocol-https-6.06.tar.gz &&
+cd LWP-Protocol-https-6.06 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/WWW-Mechanize-1.74.tar.gz &&
+tar xvzf WWW-Mechanize-1.74.tar.gz &&
+cd WWW-Mechanize-1.74 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/F/FL/FLORA/LWP-Protocol-http-SocketUnix-0.02.tar.gz &&
+tar xvzf LWP-Protocol-http-SocketUnix-0.02.tar.gz &&
+cd LWP-Protocol-http-SocketUnix-0.02 &&
+perl Makefile.PL && make test && make install &&
+wget ftp://ftp.mirrorservice.org/sites/distfiles.finkmirrors.net/Parallel-ForkManager-1.03.tar.gz &&
+tar xvzf Parallel-ForkManager-1.03.tar.gz &&
+cd Parallel-ForkManager-1.03 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/T/TU/TURNSTEP/Net-SSH-Perl-1.38.tar.gz &&
+tar xvzf Net-SSH-Perl-1.38.tar.gz &&
+cd Net-SSH-Perl-1.38 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/J/JO/JOSHUA/Net-Telnet-Cisco-1.10.tar.gz &&
+tar xvzf Net-Telnet-Cisco-1.10.tar.gz &&
+cd Net-Telnet-Cisco-1.10 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/N/NA/NANIS/Crypt-SSLeay-0.72.tar.gz &&
+tar xvzf Crypt-SSLeay-0.72.tar.gz &&
+cd Crypt-SSLeay-0.72 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/N/NJ/NJH/Net-SDP-0.07.tar.gz &&
+tar xvzf Net-SDP-0.07.tar.gz &&
+cd Net-SDP-0.07 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/Z/ZE/ZEFRAM/Time-HiRes-1.9726.tar.gz &&
+tar xvzf Time-HiRes-1.9726.tar.gz &&
+cd Time-HiRes-1.9726 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/J/JR/JROGERS/Net-Telnet-3.02.tar.gz &&
+tar xvzf Net-Telnet-3.02.tar.gz &&
+cd Net-Telnet-3.02 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/S/SA/SAPER/warnings-compat-0.07.tar.gz &&
+tar xvzf warnings-compat-0.07.tar.gz &&
+cd warnings-compat-0.07 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/S/SB/SBURKE/strict-ModuleName-0.04.tar.gz &&
+tar xvzf strict-ModuleName-0.04.tar.gz &&
+cd strict-ModuleName-0.04 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/R/RK/RKITOVER/Net-SSH2-0.53.tar.gz &&
+tar xvzf Net-SSH2-0.53.tar.gz &&
+cd Net-SSH2-0.53 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/B/BN/BNEGRAO/Net-SSH-Expect-1.09.tar.gz &&
+tar xvzf Net-SSH-Expect-1.09.tar.gz &&
+cd Net-SSH-Expect-1.09 &&
+perl Makefile.PL && make test && make install &&
+wget http://search.cpan.org/CPAN/authors/id/S/SH/SHLOMIF/IO-Socket-INET6-2.72.tar.gz &&
+tar xvzf IO-Socket-INET6-2.72.tar.gz &&
+cd IO-Socket-INET6-2.72 &&
+perl Makefile.PL && make test && make install &&
+cpan Asterisk::AGI &&
+cpan Asterisk::AMI &&
+cpan Authen::Radius &&
+cpan Bundle::CPAN &&
+cpan Bundle::LWP &&
+cpan Bundle:BI &&
+cpan Class::ErrorHandler &&
+cpan Convert::PEM &&
+cpan Crypt::DES &&
+cpan Crypt::DH &&
+cpan Crypt::RSA &&
+cpan Crypt::SSLeay &&
+cpan DBD::mysql &&
+cpan Digest::MD4 &&
+cpan Digest::MD5 &&
+cpan Digest::SHA &&
+cpan Getopt::Long &&
+cpan HTTP::Lite &&
+cpan install IO::Socket::SSL &&
+cpan install Net::FTP &&
+cpan install Net::SSH &&
+cpan install Net::SSH2 &&
+cpan install Net::SSH::Perl &&
+cpan IO::Socket &&
+cpan IO::Socket::INET6 &&
+cpan IO::Socket::SSL &&
+cpan IO::Tty &&
+cpan LWP::Protocol::http &&
+cpan LWP::Protocol::https &&
+cpan LWP::UserAgent &&
+cpan Mail::Sendmail &&
+cpan Math::GMP &&
+cpan Math::Pari &&
+cpan MIME::Base64 &&
+cpan Module::Build &&
+cpan Net::Address::IP::Local &&
+cpan Net::DNS &&
+cpan Net::DNS::SEC &&
+cpan Net::FTP &&
+cpan Net::FTP::Recursive &&
+cpan Net::OpenSSH &&
+cpan Net::Pcap &&
+cpan Net::PcapUtils &&
+cpan Net::SFTP &&
+cpan Net::SIP &&
+cpan Net::SIP::SDP &&
+cpan Net::SSH &&
+cpan Net::SSH2 &&
+cpan Net::SSH::Expect &&
+cpan Net::SSH::Perl &&
+cpan Net::SSH::Perl::Key::RSA &&
+cpan Net::SSL &&
+cpan Net::SSLeay &&
+cpan Net::Telnet &&
+cpan Net::Telnet::Cisco &&
+cpan NetPacket::Ethernet &&
+cpan NetPacket::IP &&
+cpan NetPacket::TCP &&
+cpan Parallel::ForkManager &&
+cpan Perl::Critic &&
+cpan Test::Harness::Straps &&
+cpan Time::HiRes &&
+cpan URI::Escape &&
+cpan WWW::Mechanize &&
+cpan XML::LibXML &&
+cpan XML::Simple &&
+perl -MCPAN -e 'install "Getopt::Long"' &&
+perl -MCPAN -e 'install "Net::Address::IP::Local"' &&
+perl -MCPAN -e 'install "Net::DNS"' &&
+perl -MCPAN -e 'install "Net::DNS::SEC"' &&
+perl -MCPAN -e 'install "Net::Pcap"' &&
+perl -MCPAN -e 'install "Net::PcapUtils"' &&
+perl -MCPAN -e 'install "Net::SIP"' &&
+perl -MCPAN -e 'install "Net::SSH"' &&
+perl -MCPAN -e 'install "NetPacket::Ethernet"' &&
+perl -MCPAN -e 'install "NetPacket::IP"' &&
+perl -MCPAN -e 'install "NetPacket::TCP"' &&
+perl -MCPAN -e 'install Asterisk::AGI' &&
+perl -MCPAN -e 'install Asterisk::AMI' &&
+perl -MCPAN -e 'install Authen::Radius' &&
+perl -MCPAN -e 'install Bundle::CPAN' &&
+perl -MCPAN -e 'install Bundle::LWP' &&
+perl -MCPAN -e 'install Bundle:BI' &&
+perl -MCPAN -e 'install Class::ErrorHandler' &&
+perl -MCPAN -e 'install Convert::PEM' &&
+perl -MCPAN -e 'install Crypt::DES' &&
+perl -MCPAN -e 'install Crypt::DH' &&
+perl -MCPAN -e 'install Crypt::RSA' &&
+perl -MCPAN -e 'install Crypt::SSLeay' &&
+perl -MCPAN -e 'install DBD::mysql' &&
+perl -MCPAN -e 'install Digest::MD4' &&
+perl -MCPAN -e 'install Digest::MD5' &&
+perl -MCPAN -e 'install Digest::SHA' &&
+perl -MCPAN -e 'install HTTP::Lite' &&
+perl -MCPAN -e 'install IO::Socket' &&
+perl -MCPAN -e 'install IO::Socket::INET6' &&
+perl -MCPAN -e 'install IO::Socket::SSL' &&
+perl -MCPAN -e 'install IO::Tty' &&
+perl -MCPAN -e 'install LWP::Protocol::http' &&
+perl -MCPAN -e 'install LWP::Protocol::https' &&
+perl -MCPAN -e 'install LWP::UserAgent' &&
+perl -MCPAN -e 'install Mail::Sendmail' &&
+perl -MCPAN -e 'install Math::GMP' &&
+perl -MCPAN -e 'install Math::Pari' &&
+perl -MCPAN -e 'install MIME::Base64' &&
+perl -MCPAN -e 'install Net::FTP' &&
+perl -MCPAN -e 'install Net::FTP::Recursive' &&
+perl -MCPAN -e 'install Net::OpenSSH' &&
+perl -MCPAN -e 'install Net::SFTP' &&
+perl -MCPAN -e 'install Net::SIP' &&
+perl -MCPAN -e 'install Net::SIP::SDP' &&
+perl -MCPAN -e 'install Net::SSH' &&
+perl -MCPAN -e 'install Net::SSH2' &&
+perl -MCPAN -e 'install Net::SSH::Expect' &&
+perl -MCPAN -e 'install Net::SSH::Perl' &&
+perl -MCPAN -e 'install Net::SSH::Perl::Key::RSA' &&
+perl -MCPAN -e 'install Net::SSL' &&
+perl -MCPAN -e 'install Net::SSLeay' &&
+perl -MCPAN -e 'install Net::Telnet' &&
+perl -MCPAN -e 'install Net::Telnet::Cisco' &&
+perl -MCPAN -e 'install Parallel::ForkManager' &&
+perl -MCPAN -e 'install Perl::Critic' &&
+perl -MCPAN -e 'install Time::HiRes' &&
+perl -MCPAN -e 'install URI::Escape' &&
+perl -MCPAN -e 'install WWW::Mechanize' &&
+perl -MCPAN -e 'install XML::LibXML' &&
+perl -MCPAN -e 'install XML::Simple' &&
+yum install zlib-devel -y  &&
+rm -rf mot--install--all.sh &&
+echo "FiiiiNsH-----INSTALL----TOOLS------TiToASH"
